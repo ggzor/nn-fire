55bb9b8d0c28a409a194579e4ac34b86f71ed70a
ggzor <30713864+ggzor@users.noreply.github.com>
2021-10-25 18:14:59 -0500

    Making installation quietier

--- setup-environment.sh
+++ setup-environment.sh
@@ -2,16 +2,16 @@
 
 set -euo pipefail
 
-sudo yum update -y --quiet
+sudo yum update -y --quiet &>/dev/null
 
 # Install Python 3.9 from source
 echo "Installing python3.8"
 (
   sudo amazon-linux-extras enable python3.8
-  sudo yum install -y --quiet python38 python38-devel
+  sudo yum install -y --quiet python38 python38-devel &>/dev/null
 
-  sudo yum groupinstall -y --quiet "Development Tools"
-  sudo yum install -y --quiet openssl-devel bzip2-devel libffi-devel xz-devel
+  sudo yum groupinstall -y --quiet "Development Tools" &>/dev/null
+  sudo yum install -y --quiet openssl-devel bzip2-devel libffi-devel xz-devel &>/dev/null
 
   echo "Installing poetry..."
   curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py \
@@ -20,7 +20,7 @@ echo "Installing python3.8"
 
 # Install dependencies
 (
-  sudo yum install -y --quiet git
+  sudo yum install -y --quiet git &>/dev/null
   git clone --branch complete https://github.com/ggzor/nn-fire
   cd nn-fire
 
